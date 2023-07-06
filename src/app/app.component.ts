1b4969c6f165035c4fb1ee0c35f1318df6766dd5
Alfonso-Alvar <fonsilive@gmail.com>
2023-07-06 15:45:12 +0100

    Featured: Array to save the ToDo's ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDo } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  arrToDos: ToDo[];
+
+  constructor() {
+    this.arrToDos = [];
+  }
+
+  onToDoCreated($event: any) {
+
+    this.arrToDos.push($event);
+  }
 }
