5c324a280d693670a56897f6b53835b3d717981c
Alfonso-Alvar <fonsilive@gmail.com>
2023-07-06 15:24:31 +0100

    Featured: ToDo class created and imported to the component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from 'src/app/models/todo.model';
+
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+
+  newToDo: ToDo;
+
+  constructor() {
+    this.newToDo = new ToDo();
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
